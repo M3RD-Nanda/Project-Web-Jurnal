d11dbe1f071740612bf491b2aa0af8c246f0a635
M3RD-Nanda <m.3rd.nanda@gmail.com>
2025-07-28 02:00:11 +0700

    Google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -205,6 +205,18 @@ export default async function RootLayout({
           }}
         />
 
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M9SDJCTL');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google Analytics (gtag.js) */}
         <script
           async
@@ -239,6 +251,17 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M9SDJCTL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
